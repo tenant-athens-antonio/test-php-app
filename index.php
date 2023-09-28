ca2f630d480db733f978ee81eee1682998040fed
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-09-28 13:16:10 -0600

    fix: php

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('log_errors', '1'); // Ensures that errors are logged
 
-// Set the response header
-header('Content-Type: application/json');
-
 // Log incoming requests with their headers
 error_log("Request received: " . $_SERVER['REQUEST_METHOD'] . " " . $_SERVER['REQUEST_URI']);
 foreach (getallheaders() as $name => $value) {
     error_log("$name: $value");
 }
 
+// Set the response header
+header('Content-Type: application/json');
+
 // Respond with a simple JSON message
 echo json_encode(['message' => 'Hello, World!']);
+?>
