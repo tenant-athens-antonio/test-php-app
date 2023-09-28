ba6fa6192f77de5b736ea07494ab0533fc3f82d4
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-09-28 12:43:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,20 @@
-<?php 
-require_once 'process.php'; 
-
-function writeLog($message) {
-    $formattedMessage = date('Y-m-d H:i:s') . ' - ' . $message;
-    error_log($formattedMessage, 4);
+<?php
+// Check if headers are sent by the server
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
 }
 
-// Log the incoming request
-writeLog("Incoming request: " . $_SERVER['REQUEST_METHOD'] . " " . $_SERVER['REQUEST_URI']);
+// Get all headers
 $headers = getallheaders();
-writeLog("Headers: " . json_encode($headers));
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Simple PHP App</title>
-</head>
-<body>
-    <form action="process.php" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
-        <br>
-        <button type="submit" name="submit">Submit</button>
-    </form>
-    
-    <?php if (isset($_SESSION['message'])): ?>
-    <div>
-        <?php
-            echo $_SESSION['message'];
-            unset($_SESSION['message']);
-        ?>
-    </div>
-    <?php endif; ?>
-</body>
-</html>
+// Output headers
+header('Content-Type: application/json');
+echo json_encode($headers, JSON_PRETTY_PRINT);
